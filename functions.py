a39105cccf49dab31c9717958af00398c802aaa8
Lorenzo Ressia <lorenzoressia38@gmail.com>
2022-10-05 16:38:01 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import random
+import random, os.path, pickle
 from register import *
 
 
@@ -14,7 +14,7 @@ def azar():
     id = random.randint(0, 455)
     id = validate(id, v)
     desc = random.choice(['Riego por la mañana', 'Riego por la tarde', 'Riego por la noche', 'Paseo con el tractor'])
-    importe = random.random()*1450.25
+    importe = random.random()*(random.randint(1, 500)/7)
     tipo = random.randint(0, 15)
     zona = random.randint(0, 19)
     v.append(id)
@@ -53,3 +53,58 @@ def zona(n, vec):
     for item in vec:
         if item.zona >= n:
             to_string(item)
+
+
+def matriz(vec):
+    v1 = int(input('Por favor ingrese un valor: '))
+    v2 = int(input('Por favor ingrese un valor: '))
+    fila, columna = 16, 20
+    matriz = [[0] * columna for i in range(fila)]
+    for i in vec:
+        tipo = i.tipo
+        zona = i.zona
+        matriz[tipo][zona] += 1
+    for f in range(len(matriz)):
+        for c in range(len(matriz[f])):
+            if v1 < matriz[f][c] < v2:
+                print(f'Tipo: {vec[f].tipo}| Zona: {vec[c].zona}| Cantidad: {matriz[f][c]}')
+
+def prom(vec):
+    promedio = 0
+    cant = len(vec)
+
+    for item in vec:
+        promedio += item.importe
+    promedio = promedio / cant
+
+    return promedio
+
+def generate(vec):
+    promedio = prom(vec)
+    name = input('Ingrese un nombre para el archivo(si no se encuentra será generado): ')
+    m = open(name, 'wt', encoding='utf8')
+    for item in vec:
+        if item.importe > promedio:
+            m.write(f'Id: {item.id}| Descripcion: {item.desc}| Importe: {item.importe}| Tipo: {item.tipo}| Zona: {item.zona}')
+            m.write('\n')
+    m.close()
+
+    return name
+
+
+def show(n):
+    if not os.path.exists(n):
+        print('Archivo no encontrado')
+    else:
+        m = open(n, 'rt', encoding='utf8')
+        t = os.path.getsize(n)
+        c = 0
+        # print(t)
+        while m.tell() < t:
+            linea = m.readline()
+            # linea = linea.split('\n')
+            print(linea)
+            c += 1
+            print(m.tell)
+        print(f'La cantidad de registros mostrados es de {c}')
+        m.close()
